071239725f262abd80cb5109b4fc83c095438755
alexpich <pich.alex01@gmail.com>
2020-04-02 14:11:57 -0700

    Added NewPost and NewsFeed components

--- resources/js/router.js
+++ resources/js/router.js
@@ -1,11 +1,12 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Start from "./views/Start.vue";
+import NewsFeed from "./views/NewsFeed.vue";
 
 Vue.use(VueRouter);
 
 export default new VueRouter({
     mode: "history",
 
-    routes: [{ path: "/", name: "home", component: Start }]
+    routes: [{ path: "/", name: "home", component: NewsFeed }]
 });
