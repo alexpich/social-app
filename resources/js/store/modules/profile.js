89ce833a487995c1adc10066f080aee3cbe686a5
alexpich <pich.alex01@gmail.com>
2020-04-07 14:15:28 -0700

    Refactor friend button text getter

--- resources/js/store/modules/profile.js
+++ resources/js/store/modules/profile.js
@@ -1,7 +1,6 @@
 const state = {
     user: null,
-    userStatus: null,
-    friendButtonText: null
+    userStatus: null
 };
 
 const getters = {
@@ -11,7 +10,12 @@ const getters = {
     friendship: state => {
         return state.user.data.attributes.friendship;
     },
-    friendButtonText: state => {
+    friendButtonText: (state, getters, rootState) => {
+        if (getters.friendship === null) {
+            return "Add Friend";
+        } else if (getters.friendship.data.attributes.confirmed_at === null) {
+            return "Pending Friend Request";
+        }
         return state.friendButtonText;
     }
 };
@@ -24,7 +28,6 @@ const actions = {
             .then(res => {
                 commit("setUser", res.data);
                 commit("setUserStatus", "success");
-                dispatch("setFriendButton");
             })
             .catch(error => {
                 commit("setUserStatus", "error");
@@ -36,24 +39,20 @@ const actions = {
         axios
             .post("/api/friend-request", { friend_id: friendId })
             .then(res => {
-                commit("setButtonText", "Pending Friend Request");
+                commit("setUserFriendship", res.data);
             })
             .catch(error => {
-                commit("setButtonText", "Add Friend");
+                console.log(error);
             });
-    },
-    setFriendButton({ commit, getters }) {
-        if (getters.friendship === null) {
-            commit("setButtonText", "Add Friend");
-        } else if (getters.friendship.data.attributes.confirmed_at === null) {
-            commit("setButtonText", "Pending Friend Request");
-        }
     }
 };
 
 const mutations = {
     setUser(state, user) {
         state.user = user;
+    },
+    setUserFriendship(state, friendship) {
+        state.user.data.attributes.friendship = friendship;
     },
     setUserStatus(state, status) {
         state.userStatus = status;
