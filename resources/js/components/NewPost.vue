d15a4de0582dfe739779614770750b91313b4e75
alexpich <pich.alex01@gmail.com>
2020-04-08 03:31:55 -0700

    Added ability to submit posts

--- resources/js/components/NewPost.vue
+++ resources/js/components/NewPost.vue
@@ -12,12 +12,19 @@
       </div>
       <div class="flex-1 flex mx-4">
         <input
+          v-model="postMessage"
           type="text"
           name="body"
           class="rounded-md focus:outline-none focus:shadow-outline text-sm w-full pl-4 h-8 bg-gray-200"
           placeholder="Add a post"
         />
-        <button class="bg-gray-200 ml-3 px-3 py-1 rounded-md">Post</button>
+        <transition name="fade">
+          <button
+            v-if="postMessage"
+            @click="$store.dispatch('postMessage')"
+            class="bg-gray-200 ml-2 px-3 py-1 rounded-md"
+          >Post</button>
+        </transition>
       </div>
       <div>
         <button class="flex justify-center items-center rounded-full w-10 h-10 bg-gray-200">
@@ -33,7 +40,33 @@
 </template>
 
 <script>
+import _ from "lodash";
 export default {
-  name: "NewPost"
+  name: "NewPost",
+  computed: {
+    postMessage: {
+      get() {
+        return this.$store.getters.postMessage;
+      },
+      // set(postMessage) {
+      //   this.$store.commit("updateMessage", postMessage);
+      // }
+      set: _.debounce(function(postMessage) {
+        this.$store.commit("updateMessage", postMessage);
+      }, 500)
+    }
+  }
 };
 </script>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s;
+}
+
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
--- resources/js/store/modules/posts.js
+++ resources/js/store/modules/posts.js
@@ -1,6 +1,7 @@
 const state = {
     newsPosts: null,
-    newsPostsStatus: null
+    newsPostsStatus: null,
+    postMessage: ""
 };
 const getters = {
     newsPosts: state => {
@@ -10,6 +11,9 @@ const getters = {
         return {
             newsPostsStatus: state.newsPostsStatus
         };
+    },
+    postMessage: state => {
+        return state.postMessage;
     }
 };
 const actions = {
@@ -24,6 +28,18 @@ const actions = {
             .catch(error => {
                 commit("setPostsStatus", "error");
             });
+    },
+    postMessage({ commit, state }) {
+        commit("setPostsStatus", "loading");
+        axios
+            .post("/api/posts", { body: state.postMessage })
+            .then(res => {
+                commit("pushPost", res.data);
+                commit("updateMessage", "");
+            })
+            .catch(error => {
+                commit("setPostsStatus", "error");
+            });
     }
 };
 const mutations = {
@@ -32,6 +48,12 @@ const mutations = {
     },
     setPostsStatus(state, status) {
         state.newsPostStatus = status;
+    },
+    updateMessage(state, message) {
+        state.postMessage = message;
+    },
+    pushPost(state, post) {
+        state.newsPosts.data.unshift(post);
     }
 };
 
