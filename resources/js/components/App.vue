e1961491ead030a4b4e212487ca74e0f08af3dd5
alexpich <pich.alex01@gmail.com>
2020-04-07 18:19:36 -0700

    Accept and ignore friend requests

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col flex-1 h-screen overflow-y-hidden">
+  <div class="flex flex-col flex-1 h-screen overflow-y-hidden" v-if="authUser">
     <Nav />
     <div class="flex overflow-y-hidden flex-1">
       <Sidebar />
@@ -13,6 +13,7 @@
 <script>
 import Nav from "./Nav";
 import Sidebar from "./Sidebar";
+import { mapGetters } from "vuex";
 
 export default {
   name: "App",
@@ -25,6 +26,11 @@ export default {
   },
   created() {
     this.$store.dispatch("setPageTitle", this.$route.meta.title);
+  },
+  computed: {
+    ...mapGetters({
+      authUser: "authUser"
+    })
   },
   watch: {
     $route(to, from) {
