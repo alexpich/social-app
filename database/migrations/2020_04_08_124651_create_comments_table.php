7503e2561ee6eca2a371deeb5870de0f0dfbf732
alexpich <pich.alex01@gmail.com>
2020-04-08 07:01:40 -0700

    Display comments on posts

--- database/migrations/2020_04_08_124651_create_comments_table.php
+++ database/migrations/2020_04_08_124651_create_comments_table.php
@@ -15,8 +15,8 @@ class CreateCommentsTable extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('user_id');
             $table->unsignedBigInteger('post_id');
+            $table->unsignedBigInteger('user_id');
             $table->text('body');
             $table->timestamps();
         });
